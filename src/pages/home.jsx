c3cc31a29720ec0d4d69dc9d5acb841911788d66
erxonxi <rubenruizpedreira@gmail.com>
2021-07-28 19:57:54 +0200

    test vista home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+// packages
+import React from 'react';
+import { Button, Container, Flex, Heading, Text } from '@chakra-ui/react';
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <Flex h={'100%'} w={'100%'} >
+            <Container maxWidth={'70%'} >
+                <Heading mt={8} mb={4}>
+                    Home
+                </Heading>
+                <Heading fontSize="xl" mb={4} color="teal.800">
+                    rubenruizpedreira.es
+                </Heading>
+                <Button colorScheme="teal">
+                    ACTUALIZAR WEB
+                </Button>
+            </Container>
+        </Flex>
     )
 }
 export default Home
