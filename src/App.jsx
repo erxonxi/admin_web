4c567083508314d1e459c95bfb748612c2fb25ac
erxonxi <rubenruizpedreira@gmail.com>
2021-07-28 17:54:16 +0200

    implementación rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 // packages
 import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// pages
+import Home from './pages/home';
+import Admin from './pages/admin';
+import NoFound from './pages/nofound';
 
 function App() {
   const [ count, setCount ] = useState( 0 )
 
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/admin">
+          <Admin />
+        </Route>
+        <Route>
+          <NoFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
